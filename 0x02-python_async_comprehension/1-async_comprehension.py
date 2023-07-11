40cf8d120adca8952f84e27bcba2fe05666b16f8
maiyo008 <tonymaiyo008@gmail.com>
2023-07-11 17:51:18 +0000

    Task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+1-async_comprehension.py
+Async Comprehensions
+"""
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers using
+    an async comprehension over async_generator.
+    Returns the list of 10 random numbers.
+    """
+    return [num async for num in async_generator()][:10]
