2e469c7b7c06a5b5088c5f7c44c655eba0fe89bf
maiyo008 <tonymaiyo008@gmail.com>
2023-07-11 18:23:12 +0000

    Task 0 correction by starting with yield then await

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,5 +13,5 @@ async def async_generator():
     then yield a random number between 0 and 10.
     """
     for i in range(10):
-        await asyncio.sleep(1)
         yield random.uniform(0, 10)
+        await asyncio.sleep(1)
