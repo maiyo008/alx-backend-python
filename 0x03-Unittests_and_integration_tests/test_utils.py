7b6f84866aec0d2a0bf77da1fd6db00768314dee
maiyo008 <tonymaiyo008@gmail.com>
2023-07-31 17:21:38 +0300

    Parameterize unit test to check for raised KeyError

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,5 +18,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected_result):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",), "Key 'a' not found."),
+        ({"a": 1}, ("a", "b"), "Key 'b' not found.")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        
+        self.assertEqual(str(context.exception), expected_exception_message)
+
 if __name__ == "__main__":
     unittest.main()
