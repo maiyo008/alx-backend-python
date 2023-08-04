10e200503d3d2d97cb827c97623b0e36d507a163
maiyo008 <tonymaiyo008@gmail.com>
2023-08-04 23:31:57 +0300

    unit-test GithubOrgClient._public_repos_url

--- 0x03-Unittests_and_integration_tests/README.md
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -113,3 +113,15 @@ google
 abc
 Of course, no external HTTP calls should be made.
 </Details>
+
+### Task 5. Mocking a property
+<Details>
+memoize turns methods into properties. Read up on how to mock a property (see resource).
+
+Implement the test_public_repos_url method to unit-test GithubOrgClient._public_repos_url.
+
+Use patch as a context manager to patch GithubOrgClient.org and make it return a known payload.
+
+Test that the result of _public_repos_url is the expected one based on the mocked payload.
+</Details>
+
--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -28,6 +28,23 @@ class TestGithubOrgClient(unittest.TestCase):
         )
         self.assertEqual(result, expected_result)
 
+    @patch('client.get_json')
+    def test_public_repos_url(self, mock_get_json):
+        # Set the return value for the mock get_json function
+        org_name = "google"
+        expected_org_payload = {
+            "login": org_name,
+            "repos_url": f"https://api.github.com/orgs/{org_name}/repos"
+        }
+        mock_get_json.return_value = expected_org_payload
+        client = GithubOrgClient(org_name)
+        result = client._public_repos_url
+        mock_get_json.assert_called_once_with(
+            GithubOrgClient.ORG_URL.format(org=org_name)
+        )
+        expected_url = expected_org_payload["repos_url"]
+        self.assertEqual(result, expected_url)
+
 
 if __name__ == "__main__":
     unittest.main()
